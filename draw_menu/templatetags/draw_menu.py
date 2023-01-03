b9ca9076db889b3f8f8e1e2a77a401f610548218
3ANov <3anovdev@gmail.com>
2023-01-03 20:25:42 +0300

    rewrite draw_menu for using one query db

--- draw_menu/templatetags/draw_menu.py
+++ draw_menu/templatetags/draw_menu.py
@@ -2,15 +2,15 @@ from django import template
 from django.db.models import Count
 
 from draw_menu.models import Menu, MenuItem
+from draw_menu.templatetags.templatetags_utils import group_by_menu_items
 
 register = template.Library()
 
 
 @register.inclusion_tag('draw_menu/draw_menu.html', takes_context=True)
 def draw_menu(context, menu_name):
-    menu_items = MenuItem.objects.select_related('parent_menu_item').filter(menu_id__menu_name=menu_name,
-                                                                            parent_menu_item__isnull=True)
+    menu_items = MenuItem.objects.select_related('parent_menu_item').filter(menu_id__menu_name=menu_name)
 
     return {
-       'menu_items': menu_items
+       'menu_items': group_by_menu_items(list(menu_items.values()))
     }
---
+++ draw_menu/templatetags/templatetags_utils.py
@@ -0,0 +1,27 @@
+def group_by_menu_items(menu_items):
+    menu_items.insert(0, {'parent_menu_item_id': -1, 'id': 'root'})
+
+    for item in menu_items:
+        item['children'] = []
+        item['active_flag'] = False
+
+    for i in menu_items:
+        if i['parent_menu_item_id'] is None:
+            i['parent_menu_item_id'] = 'root'
+
+    result_dict = menu_items[0]
+
+    def insert(tree, node):
+        if node["parent_menu_item_id"] == tree["id"]:
+            if "children" not in tree:
+                tree["children"] = [node]
+            else:
+                tree["children"].append(node)
+        elif "children" in tree:
+            for c in tree["children"]:
+                insert(c, node)
+
+    for record in menu_items:
+        insert(result_dict, record)
+
+    return result_dict['children']
